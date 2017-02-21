baf2371546ea19e851020086eb4ce02a607bba97
wildpachi <aybanh@gmail.com>
2017-02-20 20:37:03 -0500

    Added !hell test

--- bot.js
+++ bot.js
@@ -1,6 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var settings = require('./config/discord.js');
+
 client.on('ready', () => { console.log('Started.'); });
 
-client.login('');
+client.on('message', message => {
+  if (message.content.toLowerCase() == '!hell')
+  {
+    message.react(message.guild.emojis.filter(emoji => emoji.name.includes('foot') || emoji.name=='epicsoul').random());
+  }
+});
+
+client.login(settings.auth.token);
